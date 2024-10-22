25c8d24bdbb289eab19da0efdfa1d18b9da6699f
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-10-22 22:51:25 +0700

    [update]timeline-padding

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -34,10 +34,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10" id="experience"
+      className="w-full bg-white dark:bg-neutral-950 py-20 font-sans md:px-10" id="experience"
       ref={containerRef} 
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10 mt-" >
+      <div className="max-w-7xl mx-auto py-8 px-4 md:px-8 lg:px-10 mt-" >
         <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl font-bold">
           Changelog from my journey
         </h2>
@@ -50,7 +50,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         {data.map((item, index) => (
           <div
             key={index}
-            className="flex justify-start pt-10 md:pt-5 md:gap-10"
+            className="flex justify-start md:pt-5 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
@@ -61,7 +61,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               </h3>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
+            <div className="relative pl-20 pr-8 md:pl-4 w-full">
               <h3 className="md:hidden block text-xl mb-2 text-left font-semibold text-neutral-600 dark:text-neutral-500">
                 {item.title}
               </h3>
