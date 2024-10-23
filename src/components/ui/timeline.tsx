7139e281e49b3400bdd9a2972e736293cc4252a7
hafizhzikry24 <hafizhcool24@gmail.com>
2024-10-23 10:57:05 +0700

    [update]color

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -34,14 +34,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-gradient-to-r from-gray-800 to-gray-900 py-20 font-sans md:px-10" id="experience"
+      className="w-full bg-white py-20 font-sans md:px-10" id="experience"
       ref={containerRef} 
     >
       <div className="max-w-7xl mx-auto py-8 px-4 md:px-8 lg:px-10 mt-" >
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl font-bold">
+        <h2 className="text-lg md:text-4xl mb-4 text-[#1d377f]  max-w-4xl font-bold">
           Changelog from my journey
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+        <p className="text-neutral-900  text-sm md:text-base max-w-sm">
         I have experience in the technology field, working on diverse projects and skills development.
         </p>
       </div>
@@ -53,16 +53,16 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start md:pt-5 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-gray-200 dark:bg-gradient-to-r from-gray-800 to-gray-900 flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-400 border border-neutral-300 dark:border-neutral-700 p-2" />
-              </div>
-              <h3 className="hidden md:block text-xl md:pl-20 font-semibold text-neutral-600 dark:text-neutral-200 ">
-                {item.title}
-              </h3>
+            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-[#F3F4F6] flex items-center justify-center ">
+  <div className="h-4 w-4 rounded-full bg-[#A78BFA] border border-[#A78BFA] dark:border-[#A78BFA] p-2" />
+</div>
+<h3 className="hidden md:block text-xl md:pl-20 font-semibold text-[#1d377f]">
+  {item.title}
+</h3>
             </div>
 
             <div className="relative pl-20 pr-8 md:pl-4 w-full">
-              <h3 className="md:hidden block text-xl mb-2 text-left font-semibold text-neutral-600 dark:text-neutral-500">
+              <h3 className="md:hidden block text-xl mb-2 text-left font-semibold text-neutral-600 ">
                 {item.title}
               </h3>
               {item.content}{" "}
@@ -73,14 +73,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-pink-200 dark:via-indigo-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
               height: heightTransform,
               opacity: opacityTransform,
             }}
-            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
+            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-300 via-blue-300 to-transparent from-[0%] via-[10%] rounded-full"
           />
         </div>
       </div>
