9e8348d892939fe264ef020d3adefc2c5812a24d
hafizhzikry24 <hafizhcool24@gmail.com>
2024-10-23 11:01:47 +0700

    [update]color

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -53,7 +53,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start md:pt-5 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-[#F3F4F6] flex items-center justify-center ">
+            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-[#efe6fb] flex items-center justify-center ">
   <div className="h-4 w-4 rounded-full bg-[#A78BFA] border border-[#A78BFA] dark:border-[#A78BFA] p-2" />
 </div>
 <h3 className="hidden md:block text-xl md:pl-20 font-semibold text-[#1d377f]">
@@ -62,7 +62,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             </div>
 
             <div className="relative pl-20 pr-8 md:pl-4 w-full">
-              <h3 className="md:hidden block text-xl mb-2 text-left font-semibold text-neutral-600 ">
+              <h3 className="md:hidden block text-xl mb-2 text-left font-semibold text-[#3857ad] ">
                 {item.title}
               </h3>
               {item.content}{" "}
