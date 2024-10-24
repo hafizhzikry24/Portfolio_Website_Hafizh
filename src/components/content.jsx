98df55a171502d5f90df226fcd520a3f61969de8
hafizhzikry24 <hafizhcool24@gmail.com>
2024-10-24 15:15:48 +0700

    Revert "[add]link-preview"
    This reverts commit 76bd1ade66ad60f1d30531ae4ad504391332a292.

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,7 +3,6 @@ import Profile from "../assets/graduated.jpg";
 import { BiLogoGmail } from "react-icons/bi";
 import { useInView } from "react-intersection-observer";
 import { useLanguage } from '../LanguageContext'; // Import useLanguage
-import { LinkPreview } from "./ui/link-preview";
 
 function Content() {
   const { ref: contentRef, inView: isContentVisible } = useInView({
@@ -49,8 +48,8 @@ function Content() {
             {language === 'en' ? 'Hello! I am Muhammad Hafizh Zikry, a fresh graduate Computer Engineering Diponegoro University passionate about both backend and frontend development. I’m dedicated to mastering technologies and delivering impactful solutions.' : 'Halo! Saya Muhammad Hafizh Zikry, lulusan baru Teknik Komputer Universitas Diponegoro yang bersemangat dalam pengembangan backend dan frontend. Saya berdedikasi untuk menguasai teknologi dan memberikan solusi yang berdampak.'}
           </p>
           <div className="flex space-x-4">
-            <LinkPreview
-              url="https://www.instagram.com/hafizh.zikry/"
+            <a
+              href="https://www.instagram.com/hafizh.zikry/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -60,15 +59,15 @@ function Content() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2 "
-                className="w-6 h-6 -ml-4 text-gray-300 hover:text-purple-400 transition-transform transform hover:scale-110"
+                className="w-6 h-6 text-gray-300 hover:text-purple-400 transition-transform transform hover:scale-110"
                 viewBox="0 0 24 24"
               >
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
-            </LinkPreview>
-            <LinkPreview
-              url="https://www.linkedin.com/in/muhammad-hafizh-zikry/"
+            </a>
+            <a
+              href="https://www.linkedin.com/in/muhammad-hafizh-zikry/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -80,9 +79,9 @@ function Content() {
                 <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                 <circle cx="4" cy="4" r="2"></circle>
               </svg>
-            </LinkPreview> 
-            <LinkPreview
-              url="https://github.com/hafizhzikry24/"
+            </a>
+            <a
+              href="https://github.com/hafizhzikry24/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -97,7 +96,7 @@ function Content() {
                   d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.28.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
                 ></path>
               </svg>
-            </LinkPreview>
+            </a>
           </div>
         </div>
         {/* Image Section */}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import imagePresets from 'vite-plugin-image-presets';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    imagePresets({
-      microlink: {
-        domains: ["api.microlink.io"]
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
