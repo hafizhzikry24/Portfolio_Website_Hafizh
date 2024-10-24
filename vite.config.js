76bd1ade66ad60f1d30531ae4ad504391332a292
hafizhzikry24 <hafizhcool24@gmail.com>
2024-10-24 15:09:17 +0700

    [add]link-preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import imagePresets from 'vite-plugin-image-presets';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    imagePresets({
+      microlink: {
+        domains: ["api.microlink.io"]
+      },
+    }),
+  ],
+});
