c355c7b9383df9cc5803b4ff93490a07b2360d61
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-10-24 21:36:33 +0700

    [add]beautiful-section

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Profile from "../assets/graduated.jpg";
 import { BiLogoGmail } from "react-icons/bi";
 import { useInView } from "react-intersection-observer";
-import { useLanguage } from '../LanguageContext'; // Import useLanguage
+import { useLanguage } from "../LanguageContext"; // Import useLanguage
 import js from "../assets/js.png";
 import reactImg from "../assets/react.png";
 import css from "../assets/css.png";
@@ -13,14 +13,14 @@ import html from "../assets/html .png";
 import tailwind from "../assets/tailwind.png";
 import github from "../assets/github.png";
 import laravel from "../assets/laravel.png";
-
+import CV from "../assets/CV_Muhammad Hafizh Zikry.pdf";
 
 function Content() {
   const { ref: contentRef, inView: isContentVisible } = useInView({
     triggerOnce: false, // Allows fade-in animation on both scroll up and down
     threshold: 0.1,
   });
-  
+
   const settings = {
     dots: false,
     infinite: true,
@@ -45,15 +45,15 @@ function Content() {
 
   return (
     <section
-      className="text-gray-500 body-font bg-gradient-to-r py-20 sm:py-24 from-gray-800 to-gray-900"
+      className="text-gray-500 body-font bg-gradient-to-r py-24 from-gray-800 to-gray-900"
       id="content"
     >
       <div
         ref={contentRef}
         className={`container mx-auto flex flex-col-reverse lg:flex-row items-center py-12 px-6 lg:px-12 transition-all duration-1000 ease-in-out transform ${
           isContentVisible
-            ? 'opacity-100 translate-y-0'
-            : 'opacity-0 translate-y-10'
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
         }`}
       >
         {/* Text Section */}
@@ -67,7 +67,7 @@ function Content() {
               <line x1="0" y1="5" x2="10" y2="5" />
             </svg>
             <h2 className="text-xl sm:text-2xl font-light text-gray-300 uppercase ">
-              {language === 'en' ? 'My name is': 'Nama Saya'} 
+              {language === "en" ? "My name is" : "Nama Saya"}
             </h2>
           </div>
           <h3 className="text-3xl sm:text-5xl font-bold text-white mb-4 animate-pulse sm:animate-bounce">
@@ -76,9 +76,11 @@ function Content() {
             </span>
           </h3>
           <p className="text-md md:text-xl text-gray-300 mb-6 text-justify transition-transform transform hover:scale-110">
-            {language === 'en' ? 'Hello! I am Muhammad Hafizh Zikry, a fresh graduate Computer Engineering Diponegoro University passionate about both backend and frontend development. I’m dedicated to mastering technologies and delivering impactful solutions.' : 'Halo! Saya Muhammad Hafizh Zikry, lulusan baru Teknik Komputer Universitas Diponegoro yang bersemangat dalam pengembangan backend dan frontend. Saya berdedikasi untuk menguasai teknologi dan memberikan solusi yang berdampak.'}
+            {language === "en"
+              ? "Hello! I am Muhammad Hafizh Zikry, a fresh graduate Computer Engineering Diponegoro University passionate about both backend and frontend development. I’m dedicated to mastering technologies and delivering impactful solutions."
+              : "Halo! Saya Muhammad Hafizh Zikry, lulusan baru Teknik Komputer Universitas Diponegoro yang bersemangat dalam pengembangan backend dan frontend. Saya berdedikasi untuk menguasai teknologi dan memberikan solusi yang berdampak."}
           </p>
-          <div className="flex space-x-4">
+          <div className="flex items-center space-x-4 mb-3">
             <a
               href="https://www.instagram.com/hafizh.zikry/"
               target="_blank"
@@ -127,6 +129,11 @@ function Content() {
                   d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.28.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
                 ></path>
               </svg>
+            </a>
+            <a href={CV} download="CV_Muhammad_Hafizh_Zikry.pdf">
+              <button className="transform hover:-translate-y-1 transition duration-400 inline-flex h-11 animate-shimmer items-center justify-center rounded-3xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 focus:outline-none focus:ring-1 focus:ring-slate-600 focus:ring-offset-1 focus:ring-offset-slate-100">
+                Download CV
+              </button>
             </a>
           </div>
         </div>
