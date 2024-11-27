330f325566d90440a690e6c810bc86a395662ee7
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-11-27 20:34:25 +0700

    [add]robots.txt-sitemap-xml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap'; // Import plugin sitemap
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      // URL situs Anda (ganti dengan URL asli)
+      siteUrl: 'https://zikkdev.vercel.app',
+      // Menentukan apakah sitemap harus diperbarui setiap kali situs dibangun
+      changefreq: 'daily',
+      priority: 0.7,
+    }),
+  ],
+});
