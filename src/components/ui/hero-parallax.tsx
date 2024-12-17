0ba5b0fa928eb746cb2ee13b12721ea663463103
hafizhzikry24 <hafizhcool24@gmail.com>
2024-12-17 13:54:41 +0700

    [update]parallax-h

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -56,7 +56,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[327vh] sm:h-[240vh] md:h-[230vh] lg:h-[240vh] xl:h-[320vh] 2xl:h-[320vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d] bg-black"
+      className="h-[327vh] sm:h-[240vh] md:h-[262vh] lg:h-[240vh] xl:h-[320vh] 2xl:h-[320vh] py-48 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d] bg-black"
     >
       <Header />
       <motion.div
