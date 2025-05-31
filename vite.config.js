70779063bf82907462194b7bac1f81a36171c98b
hafizhzikry24 <hafizhcool24@gmail.com>
2025-05-31 15:13:39 +0700

    fix:depedencies-vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap'; // Import plugin sitemap
+import sitemap from 'vite-plugin-sitemap';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    {
+      name: 'ensure-dist-folder',
+      buildStart() {
+        const distPath = path.resolve(__dirname, 'dist');
+        if (!fs.existsSync(distPath)) {
+          fs.mkdirSync(distPath);
+        }
+      },
+    },
     sitemap({
-      // URL situs Anda (ganti dengan URL asli)
       siteUrl: 'https://zikkdev.vercel.app',
-      // Menentukan apakah sitemap harus diperbarui setiap kali situs dibangun
       changefreq: 'daily',
       priority: 0.7,
     }),
   ],
   build: {
-    sourcemap: true,  // Menambahkan konfigurasi sourcemap untuk produksi
+    sourcemap: true,
   },
 });
